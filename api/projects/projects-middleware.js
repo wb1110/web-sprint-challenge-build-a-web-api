603ae3924cbdf09685afee7b997c80a42f150420
wb1110 <strengthcoachwb@gmail.com>
2022-01-14 08:31:57 -0600

    middleware ID added

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -1,1 +1,18 @@
 // add middlewares here related to projects
+const Projects = require('./projects-model');
+
+async function checkId (req, res, next) {
+  console.log('checking id')
+  try {
+    const projectId = await Projects.get(req.params.id)
+    if (projectId) {
+      next()
+    } else {
+      next(res.status(404).json({ message: "No project with that ID exists!" }))
+    }
+  } catch(err) {
+    next(err)
+  }
+}
+
+module.exports = checkId
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,12 +1,21 @@
 // Write your "projects" router here!
 const Projects = require('./projects-model');
 const express = require('express');
+const checkId = require('./projects-middleware');
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
   Projects.get()
   .then(projects => {
     console.log(projects)
+    res.json(projects)
+  })
+  .catch(next)
+})
+
+router.get("/:id", checkId, (req, res, next) => {
+  Projects.get(req.params.id)
+  .then(projects => {
     res.json(projects)
   })
   .catch(next)
