3f380935ff18b43790f754e218b989c47bcee506
wb1110 <strengthcoachwb@gmail.com>
2022-01-14 07:35:31 -0600

    first endpoint

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,15 @@
 // Write your "projects" router here!
+const Projects = require('./projects-model');
+const express = require('express');
+const router = express.Router();
+
+router.get("/", (req, res, next) => {
+  Projects.get()
+  .then(projects => {
+    console.log(projects)
+    res.json(projects)
+  })
+  .catch(next)
+})
+
+module.exports = router
--- api/server.js
+++ api/server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const server = express();
+const projectRouter = require('./projects/projects-router')
+
 server.use(express.json())
+server.use('/api/projects', projectRouter)
 
 // Configure your server here
 // Build your actions router in /api/actions/actions-router.js
