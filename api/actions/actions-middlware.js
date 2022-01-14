2008511ebc88cec2c1fb690a406da1488f3ccb2b
wb1110 <strengthcoachwb@gmail.com>
2022-01-14 09:58:00 -0600

    post done

--- api/actions/actions-middlware.js
+++ api/actions/actions-middlware.js
@@ -1,1 +1,45 @@
 // add middlewares here related to actions
+const Actions = require('./actions-model');
+
+async function checkId (req, res, next) {
+  try {
+    const projectId = await Actions.get(req.params.id)
+    if (projectId) {
+      next()
+    } else {
+      next(res.status(404).json({ message: "No project with that ID exists!" }))
+    }
+  } catch(err) {
+    next(err)
+  }
+}
+
+async function checkFields (req, res, next) {
+  try {
+    if (req.body.notes && req.body.description && req.body.project_id) {
+      next()
+    } else {
+      next(res.status(400).json({ message: "Missing required field!" }))
+    }
+  } catch(err) {
+    next(err)
+  }
+}
+
+async function updateFields (req, res, next) {
+  try {
+    if (req.body.name && req.body.description && (Object.prototype.hasOwnProperty.call(req.body, "completed"))) {
+      next()
+    } else {
+      next(res.status(400).json({ message: "Missing required field!" }))
+    }
+  } catch(err) {
+    next(err)
+  }
+}
+
+module.exports = {
+  checkId,
+  checkFields,
+  updateFields
+}
--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,1 +1,47 @@
 // Write your "actions" router here!
+const Actions = require('./actions-model');
+const express = require('express');
+const { checkId, checkFields, updateFields } = require('./actions-middlware');
+const router = express.Router();
+
+router.get("/", (req, res, next) => {
+  Actions.get()
+  .then(actions => {
+    res.json(actions)
+  })
+  .catch(next)
+})
+
+router.get("/:id", checkId, (req, res, next) => {
+  Actions.get(req.params.id)
+  .then(actions => {
+    res.json(actions)
+  })
+  .catch(next)
+})
+
+router.post("/", checkFields, (req, res, next) => {
+  Actions.insert(req.body)
+  .then(action => {
+    res.json(action)
+  })
+  .catch(next)
+})
+
+router.put("/:id", checkId, updateFields, (req, res, next) => {
+  Actions.update(req.params.id, req.body)
+  .then(actionUpdate => {
+    res.json(actionUpdate)
+  })
+  .catch(next)
+})
+
+router.delete("/:id", checkId, (req, res, next) => {
+  Actions.remove(req.params.id)
+  .then(actionDelete => {
+    res.json(actionDelete)
+  })
+  .catch(next)
+})
+
+module.exports = router
